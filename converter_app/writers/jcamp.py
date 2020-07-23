57c6c7fcc8940120fc901e64b1853942e538e783
Jochen Klar <mail@jochenklar.de>
2020-07-23 14:42:12 +0200

    Add write_xypoints to JcampWriter

--- converter_app/writers/jcamp.py
+++ converter_app/writers/jcamp.py
@@ -7,7 +7,7 @@ from . import Writer
 
 class JcampWriter(Writer):
 
-    nline = 8
+    nline = 12
 
     data_types = (
         'INFRARED SPECTRUM',
@@ -70,7 +70,7 @@ class JcampWriter(Writer):
         deltax = (float(lastx) - float(firstx)) / (npoints - 1)
 
         # find YFACTOR, MINY, and MAXY
-        minx = sys.float_info.max
+        miny = sys.float_info.max
         maxy = sys.float_info.min
         max_decimal = 0
         for i, string in enumerate(y):
@@ -79,7 +79,7 @@ class JcampWriter(Writer):
             index = string.index('.')
             decimal = len(string) - index - 1
 
-            minx = min(minx, value)
+            miny = min(miny, value)
             maxy = max(maxy, value)
             max_decimal = max(max_decimal, decimal)
         yfactor = 10**(-decimal)
@@ -90,7 +90,7 @@ class JcampWriter(Writer):
             'LASTX': lastx,
             'MINX': firstx,
             'MAXX': lastx,
-            'MINY': minx,
+            'MINY': miny,
             'MAXY': maxy,
             'NPOINTS': npoints,
             'DELTAX': deltax,
@@ -110,17 +110,60 @@ class JcampWriter(Writer):
             x = float(firstx) + i * deltax
 
             line = str(x)
-            for j in range(self.nline + 1):
+            for j in range(self.nline):
                 if i + j < npoints:
                     string = y[i+j]
                     index = string.index('.')
                     decimal = len(string) - index - 1
-                    line += '\t' + string.replace('.', '') + (max_decimal - decimal) * '0'
+                    line += ',' + string.replace('.', '') + (max_decimal - decimal) * '0'
 
             self.buffer.write(line + os.linesep)
 
         # write the end
-        self.buffer.write('##END=$$ End of the data block')
+        self.buffer.write('##END=$$ End of the data block' + os.linesep)
 
     def write_xypoints(self, metadata, data):
-        pass
+        points = data.get('points')
+        firstx = points[0][0]
+        firsty = points[0][1]
+        lastx = points[-1][0]
+        npoints = len(points)
+
+        # find MINX, MAXX, MINY, MAXY
+        minx = sys.float_info.max
+        maxx = sys.float_info.min
+        miny = sys.float_info.max
+        maxy = sys.float_info.min
+        for point in points:
+            x, y = float(point[0]), float(point[1])
+
+            minx = min(minx, x)
+            maxx = max(maxx, x)
+            miny = min(miny, y)
+            maxy = max(maxy, y)
+
+        # update metadata with xydata specific values
+        metadata.update({
+            'FIRSTX': firstx,
+            'LASTX': lastx,
+            'MINX': minx,
+            'MAXX': maxx,
+            'MINY': miny,
+            'MAXY': maxy,
+            'NPOINTS': npoints,
+            'FIRSTY': firsty,
+            'XYPOINTS': '(XY..XY)'
+        })
+
+        # write the metadata
+        for key, value in metadata.items():
+            if value is not None:
+                self.buffer.write('##{}={}'.format(key, value) + os.linesep)
+
+        # write the xypoints
+        for point in points:
+            line = '{},{}'.format(*point)
+            self.buffer.write(line + os.linesep)
+
+        # write the end
+        self.buffer.write('##END=$$ End of the data block' + os.linesep)
