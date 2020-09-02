5a67c9e381d480180ae6d58386358ee0d1cf0707
Magdalena Noffke <magdalena.noffke@googlemail.com>
2020-09-02 15:32:55 +0200

    Allow malformed csv

--- converter_app/readers/csv.py
+++ converter_app/readers/csv.py
@@ -1,11 +1,18 @@
 import csv
 import io
+import re
 import logging
+import pandas
 import string
 
 from .base import Reader
 
 logger = logging.getLogger(__name__)
+
+PATTERNS = {
+    'text': re.compile(r'[A-Za-z]{2,}'),                 # two or more chars in row
+    'floats': re.compile(r'(\d+[,.]*\d*[eE+\-\d]*)\S*')  # e.g. 1.00001E-10
+}
 
 
 class CSVReader(Reader):
@@ -28,18 +35,73 @@ class CSVReader(Reader):
         io_string = io.StringIO(self.file_reader.read().decode(self.encoding))
         reader = csv.reader(io_string, self.dialect)
 
-        columns = []
-        rows = []
+        try:
+            pandas.read_csv(io_string)
+            columns = []
+            rows = []
+            for row in reader:
+                if not columns:
+                    columns = [{
+                        'key': str(idx),
+                        'name': string.ascii_uppercase[idx]
+                    } for idx, value in enumerate(row)]
+                rows.append(row)
+
+            return [{
+                'header': [],
+                'columns': columns,
+                'rows': rows
+            }]
+        except pandas.errors.ParserError:
+            return self.get_data_from_malformed_csv(io_string, reader)
+
+    def get_data_from_malformed_csv(self, io_string, reader):
+        io_string.seek(0)
+        tables = []
+        self.append_table(tables)
+        current_length = None
+
         for row in reader:
-            if not columns:
-                columns = [{
+            as_string = ' '.join(row)
+            text_match = PATTERNS['text'].search(as_string)
+
+            if text_match:
+                if current_length is not None:
+                    self.append_table(tables)
+
+                # append header line to last table
+                tables[-1]['header'].append(as_string)
+                current_length = None
+
+            else:
+                # try to match columns of floats
+                float_match = PATTERNS['floats'].findall(as_string)
+                if float_match:
+                    float_match = [float_str.replace(',', '.')
+                                   for float_str in float_match]
+                    if current_length == len(float_match):
+                        # the current number of columns is the same
+                        tables[-1]['rows'].append(float_match)
+
+                    elif current_length is not None:
+                        self.append_table(tables)
+
+                    tables[-1]['rows'].append(float_match)
+                    current_length = len(float_match)
+
+        for table in tables:
+            if table['rows']:
+                table['columns'] = [{
                     'key': str(idx),
                     'name': string.ascii_uppercase[idx]
-                } for idx, value in enumerate(row)]
-            rows.append(row)
+                } for idx, value in enumerate(table['rows'][0])]
 
-        return [{
+        return tables
+
+
+    def append_table(self, tables):
+        tables.append({
             'header': [],
-            'columns': columns,
-            'rows': rows
-        }]
+            'columns': [],
+            'rows': []
+        })
