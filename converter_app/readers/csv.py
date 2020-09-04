87d014e2e17b1e0c3aa9fc13b8e79a28f8deceea
Jochen Klar <mail@jochenklar.de>
2020-09-04 16:03:56 +0200

    Refactor ascii and csv reader

--- converter_app/readers/csv.py
+++ converter_app/readers/csv.py
@@ -1,93 +1,74 @@
+import copy
 import csv
 import io
-import re
 import logging
-import pandas
 import string
 
 from .base import Reader
 
 logger = logging.getLogger(__name__)
 
-PATTERNS = {
-    'text': re.compile(r'[A-Za-z]{2,}'),                 # two or more chars in row
-    'floats': re.compile(r'(\d+[,.]*\d*[eE+\-\d]*)\S*')  # e.g. 1.00001E-10
-}
-
 
 class CSVReader(Reader):
     identifier = 'csv_reader'
+    priority = 100
 
     def check(self):
-        file_string, self.encoding = self.peek_ascii()
+        file_string, encoding = self.peek_ascii()
 
         try:
-            self.dialect = csv.Sniffer().sniff(file_string, delimiters=';,\t')
+            dialect = csv.Sniffer().sniff(file_string, delimiters=';,\t')
         except csv.Error:
             result = False
         else:
+            io_string = io.StringIO(self.file_reader.read().decode(encoding))
+            self.lines = copy.copy(io_string)
+            self.reader = csv.reader(io_string, dialect)
             result = True
 
         logger.debug('result=%s', result)
         return result
 
     def get_data(self):
-        io_string = io.StringIO(self.file_reader.read().decode(self.encoding))
-        reader = csv.reader(io_string, self.dialect)
-
-        try:
-            pandas.read_csv(io_string)
-            columns = []
-            rows = []
-            for row in reader:
-                if not columns:
-                    columns = [{
-                        'key': str(idx),
-                        'name': string.ascii_uppercase[idx]
-                    } for idx, value in enumerate(row)]
-                rows.append(row)
-
-            return [{
-                'header': [],
-                'columns': columns,
-                'rows': rows
-            }]
-        except pandas.errors.ParserError:
-            return self.get_data_from_malformed_csv(io_string, reader)
-
-    def get_data_from_malformed_csv(self, io_string, reader):
-        io_string.seek(0)
         tables = []
         self.append_table(tables)
-        current_length = None
 
-        for row in reader:
-            as_string = ' '.join(row)
-            text_match = PATTERNS['text'].search(as_string)
+        previous_shape = None
+        previous_row = None
+        for line, row in zip(self.lines, self.reader):
+            shape = self.get_shape(row)
 
-            if text_match:
-                if current_length is not None:
+            if 's' in shape:
+                # there is a string in this row, this cant be the table
+
+                if tables[-1]['rows']:
+                    # if a table is already there, this must be a new header
                     self.append_table(tables)
 
-                # append header line to last table
-                tables[-1]['header'].append(as_string)
-                current_length = None
+                tables[-1]['header'].append(line)
+
+            elif 'f' in shape:
+                if tables[-1]['rows'] and shape != previous_shape:
+                    # start a new table if the shape has changed
+                    self.append_table(tables)
+
+                elif tables[-1]['rows'] == [] and [bool(s) for s in shape] == [bool(s) for s in previous_shape]:
+                    # move the previous row with a "similar" shape from the header to the table
+                    tables[-1]['header'].pop()
+                    values = [previous_row[i] for i, value in enumerate(previous_shape) if value != '']
+                    tables[-1]['rows'].append(values)
+
+                # this row has floats but no strings, this is the "real" table
+                values = [row[i] for i, value in enumerate(shape) if value == 'f']
+                tables[-1]['rows'].append(values)
 
             else:
-                # try to match columns of floats
-                float_match = PATTERNS['floats'].findall(as_string)
-                if float_match:
-                    float_match = [float_str.replace(',', '.')
-                                   for float_str in float_match]
-                    if current_length == len(float_match):
-                        # the current number of columns is the same
-                        tables[-1]['rows'].append(float_match)
+                # empty lines are preserved for the header
+                tables[-1]['header'].append(line)
 
-                    elif current_length is not None:
-                        self.append_table(tables)
-
-                    tables[-1]['rows'].append(float_match)
-                    current_length = len(float_match)
+            # store shape and row for the next iteration
+            previous_shape = shape
+            previous_row = row
 
         for table in tables:
             if table['rows']:
@@ -98,10 +79,22 @@ class CSVReader(Reader):
 
         return tables
 
-
     def append_table(self, tables):
         tables.append({
             'header': [],
             'columns': [],
             'rows': []
         })
+
+    def get_shape(self, row):
+        shape = []
+        for cell in row:
+            if cell.strip() == '':
+                shape.append('')
+            else:
+                try:
+                    float(cell.replace(',', '.'))
+                    shape.append('f')
+                except ValueError:
+                    shape.append('s')
+        return shape
