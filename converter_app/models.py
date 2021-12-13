6b7506c50ff2f8e9725e6cacb741179d5b566c84
Jochen Klar <mail@jochenklar.de>
2021-12-13 11:12:14 +0100

    Fix delete profile and refactor Profile model

--- converter_app/models.py
+++ converter_app/models.py
@@ -11,8 +11,9 @@ logger = logging.getLogger(__name__)
 
 class Profile(object):
 
-    def __init__(self, profile_data, profile_id=None):
+    def __init__(self, profile_data, client_id, profile_id=None):
         self.data = profile_data
+        self.client_id = client_id
         self.id = profile_id
 
     def clean(self):
@@ -44,8 +45,8 @@ class Profile(object):
         if not self.errors:
             return True
 
-    def save(self, client_id):
-        profiles_path = Path(current_app.config['PROFILES_DIR']).joinpath(client_id)
+    def save(self):
+        profiles_path = Path(current_app.config['PROFILES_DIR']).joinpath(self.client_id)
         profiles_path.mkdir(parents=True, exist_ok=True)
 
         if self.id is None:
@@ -57,7 +58,7 @@ class Profile(object):
             json.dump(self.data, fp, sort_keys=True, indent=4)
 
     def delete(self):
-        profiles_path = Path(current_app.config['PROFILES_DIR'])
+        profiles_path = Path(current_app.config['PROFILES_DIR']).joinpath(self.client_id)
 
         file_path = profiles_path.joinpath(self.id).with_suffix('.json')
         if file_path.is_file():
@@ -78,7 +79,7 @@ class Profile(object):
             for file_path in Path.iterdir(profiles_path):
                 profile_id = str(file_path.with_suffix('').name)
                 profile_data = json.loads(file_path.read_text())
-                yield cls(profile_data, profile_id)
+                yield cls(profile_data, client_id, profile_id)
         else:
             return []
 
@@ -95,6 +96,6 @@ class Profile(object):
         file_path = profiles_path.joinpath(profile_id).with_suffix('.json')
         if file_path.is_file():
             profile_data = json.loads(file_path.read_text())
-            return cls(profile_data, profile_id)
+            return cls(profile_data, client_id, profile_id)
         else:
             return False
