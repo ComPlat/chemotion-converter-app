0a8b4729fd128738f5e956916de6fbdadf19da5d
Jochen Klar <mail@jochenklar.de>
2022-02-02 17:55:40 +0100

    Add DTA reader

--- converter_app/readers/dta.py
+++ converter_app/readers/dta.py
@@ -1,19 +1,16 @@
 import logging
 import re
 
+from pathlib import Path
+
 from .base import Reader
 
 logger = logging.getLogger(__name__)
 
-PATTERNS = {
-    'text': re.compile(r'[A-Za-z]{2,}'),                   # two or more chars in row
-    'floats': re.compile(r'(-?\d+[,.]*\d*[eE+\-\d]*)\S*')  # e.g. 1.00001E-10
-}
 
-
-class AsciiReader(Reader):
-    identifier = 'ascii_reader'
-    priority = 1000
+class DtaReader(Reader):
+    identifier = 'dta_reader'
+    priority = 10
 
     def check(self):
         logger.debug('file_name=%s content_type=%s mime_type=%s encoding=%s',
@@ -21,47 +18,43 @@ class AsciiReader(Reader):
 
         if self.encoding == 'binary':
             result = False
-        else:
+        elif Path(self.file_name).suffix.lower() == '.dta' and self.mime_type == 'text/plain':
             result = True
+        else:
+            result = False
 
         logger.debug('result=%s', result)
         return result
 
     def get_data(self):
+        count = 0
         tables = []
         self.append_table(tables)
 
         # loop over lines of the file
-        previous_count = None
         for line in self.file.readlines():
             row = line.decode(self.encoding).rstrip()
-            count = None
 
-            # try to match text for the header
-            text_match = PATTERNS['text'].search(row)
-            if text_match:
+            if row.startswith('\t'):
+                count += 1
+
+                if count < 3:
+                    # add the first 2 rows to the header
+                    tables[-1]['header'].append(row)
+                else:
+                    tables[-1]['rows'].append([self.get_value(value) for value in row.split()])
+            else:
+                # this is the header
                 if tables[-1]['rows']:
                     # if a table is already there, this must be a new header
                     self.append_table(tables)
 
+                    # reset the row count
+                    count = 0
+
                 # append header line to last table
                 tables[-1]['header'].append(row)
-            else:
-                # try to match columns of floats
-                row = row.replace('n.a.','0')
-                float_match = PATTERNS['floats'].findall(row)
-                if float_match:
-                    # replace , by . in floats
-                    float_match = [float_str.replace(',', '.') for float_str in float_match]
-                    count = len(float_match)
 
-                    if tables[-1]['rows'] and count != previous_count:
-                        # start a new table if the shape has changed
-                        self.append_table(tables)
-
-                    tables[-1]['rows'].append(float_match)
-
-            previous_count = count
 
         # loop over tables and append rows
         for table in tables:
@@ -79,3 +72,9 @@ class AsciiReader(Reader):
             'columns': [],
             'rows': []
         })
+
+    def get_value(self, value):
+        try:
+            return float(value.replace(',', '.'))
+        except ValueError:
+            return value
