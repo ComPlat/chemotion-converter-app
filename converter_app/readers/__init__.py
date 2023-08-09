7cb3c78c2f28db3d2648cf6f48d8ccccdb25df3b
Martin Starman <m.starman@live.com>
2023-08-09 12:11:45 +0200

    Added EBL reader

--- converter_app/readers/__init__.py
+++ converter_app/readers/__init__.py
@@ -18,6 +18,7 @@ from .sec import SecReader
 from .sem import SemReader
 from .gcd import GcdReader
 from .old_excel import OldExcelReader
+from .ebl import EblReader
 
 logger = logging.getLogger(__name__)
 
@@ -72,3 +73,4 @@ registry.register(AscZipReader)
 registry.register(SemReader)
 registry.register(GcdReader)
 registry.register(OldExcelReader)
+registry.register(EblReader)
