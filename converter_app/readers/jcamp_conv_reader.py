7dd82c6475761973a424d406785581fa99b5ba2f
Martin Starman <m.starman@live.com>
2024-01-17 16:07:38 +0100

    Reset Header of Branch #88

--- converter_app/readers/jcamp_conv_reader.py
+++ converter_app/readers/jcamp_conv_reader.py
@@ -4,6 +4,7 @@ from numpy import ndarray
 
 from .base import Reader
 from jcamp import jcamp_read
+
 logger = logging.getLogger(__name__)
 
 
@@ -17,24 +18,16 @@ class JcampReader(Reader):
         logger.debug('result=%s', result)
         return result
 
-    def add_to_meta(self, table, k, src):
-        if isinstance(src, list):
-            src_iter = enumerate(src)
-        elif isinstance(src, dict):
-            src_iter = src.items()
-        elif isinstance(src, ndarray) or src is None:
-            return
-        else:
-            table['metadata'][k] = str(src)[:255]
-            return
-        for (k,v) in src_iter:
-            self.add_to_meta(table, k, v)
-
     def get_tables(self):
         tables = []
         table = self.append_table(tables)
         res = jcamp_read(self.file.fp)
-        self.add_to_meta(table, None, res)
+        for (k,v) in res.items():
+            if isinstance(v, ndarray):
+                pass
+            else:
+                table['metadata'][k] = str(v)
+
         table['metadata']['rows'] = str(0)
         table['metadata']['columns'] = str(0)
         return tables
