04c1f8e5515fd0b43b16e260c661e0ecfbc4060a
Adrian Herrmann <headri@web.de>
2024-10-07 13:04:58 +0200

    Update afm.py
    correction and optimization

--- converter_app/readers/afm.py
+++ converter_app/readers/afm.py
@@ -41,55 +41,17 @@ class AfmReader(Reader):
                 table['header'].append(row[2:])
                 header = False
             else:
-                k_v = row[1:].split(':')
+                k_v = [x.strip() for x in row[1:].split(':', 1)]
                 table['metadata'].add_unique(k_v[0], k_v[-1])
 
         extracted_table = self.append_table(tables)
-        extracted_table['metadata'] = self._extract_information_afm(lines)
+        for term in search_terms_matrix:
+            value = table['metadata'].get(term[2]) if table['metadata'].get(term[2]) is not None else ''
+            extracted_table['metadata'].add_unique(term[0], value)
+
+        extracted_table['metadata'] = convert_units(extracted_table['metadata'], 2)
 
         return tables
-
-    @staticmethod
-    def _extract_information_afm(lines):
-        # Create the table with search_terms_matrix
-        table = [[term[0], ''] for term in search_terms_matrix]
-
-        # Simplify the file content
-        end_marker = b'*File list end'
-        try:
-            end_index = next(i for i, line in enumerate(lines) if end_marker in line)
-            lines = lines[:end_index]
-        except StopIteration:
-            return {}
-
-        # Converting the lines into a list of strings
-        simplified_data = [line.decode('utf-8', errors='ignore').strip() for line in lines]
-
-        # Search the simplified data list for the search terms
-        for term in search_terms_matrix:
-            search_term = term[2]
-            if search_term:
-                AfmReader._find_search_term(search_term, simplified_data, table, term)
-
-        # Create info_dict from table
-        info_dict = {row[0]: row[1] for row in table}
-        info_dict = convert_units(info_dict, 2)
-        return info_dict
-
-    @staticmethod
-    def _find_search_term(search_term, simplified_data, table, term):
-        for line in simplified_data:
-            if search_term in line:
-                # Finde den Wert nach dem Suchbegriff
-                match = re.search(rf'{re.escape(search_term)}\s*:\s*(.*)', line)
-                if match:
-                    value = match.group(1).strip()
-                    # Finde die Zeile in der Tabelle, die dem Suchbegriff entspricht
-                    for row in table:
-                        if row[0] == term[0]:
-                            row[1] = value
-                            break
-                break
 
 
 Readers.instance().register(AfmReader)
