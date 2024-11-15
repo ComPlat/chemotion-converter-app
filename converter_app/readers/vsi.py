b7032741a55bd74be90ae87fc540244706575253
Adrian Herrmann <headri@web.de>
2024-11-15 10:52:11 +0100

    Changed VIS reader
    used XML Reader as base + minor changes in XML Reader

--- converter_app/readers/vsi.py
+++ converter_app/readers/vsi.py
@@ -1,52 +1,87 @@
 import logging
 
+import xml.etree.ElementTree as ET
+
+from converter_app.models import File
 from converter_app.readers.helper.reader import Readers
-from converter_app.readers.helper.base import Reader
+from converter_app.readers.xml_reader import XMLReader
 
 logger = logging.getLogger(__name__)
 
 
-class VsiReader(Reader):
+class VsiReader(XMLReader):
     """
-        Implementation of the Vsm Reader. It extends converter_app.readers.helper.base.Reader
+        Implementation of the Vsm Reader. It extends converter_app.readers.XMLReader
     """
     identifier = 'vsi_reader'
-    priority = 10
+    priority = 8
+
+    def __init__(self, file: File):
+        super().__init__(file)
+        self.data_key = ''
 
     def check(self):
-        return self.file.suffix.lower() == '.xml'
+        if not super().check():
+            return False
+        return self.file.string.find('<![CDATA[')
+
+    def handle_node(self, node: ET.Element, xml_path: str, node_name: str):
+        """
+        This method can be overridden to handle special nodes separately.
+
+        :param node: XML node Object
+        :param xml_path: Path in global XML-file to this node
+        :param node_name: Name of the Node
+        """
+        if node_name == 'Data':
+            if node.text.strip() == '':
+                if node.attrib.get('typename') == 'FixedSpacing2D':
+                    self.data_key = node.attrib.get('key')
+                else:
+                    value, unit = self.get_value_with_unit(node)
+                    self._table['metadata'][node.attrib.get('key')] = value + unit
+            else:
+                self._table['metadata'][node.attrib.get('key')] = node.text
+
+        if node_name in ('Dim1Spacing','Dim2Spacing'):
+            value, unit = self.get_value_with_unit(node)
+            self._table['metadata'][self.data_key + '.' + node_name] = value + unit
+
+    def get_value_with_unit(self, node):
+        """
+        This method handles Value + Unit sub notes
+
+        :param node: XML node Object
+        :returns: value and unit if available
+        """
+        try:
+            value = node.find('Value').text.strip()
+        except AttributeError:
+            value = ''
+        try:
+            unit = ' ' + node.find('Unit').text.strip()
+        except AttributeError:
+            unit = ''
+        return value, unit
+
+    def ignore_node(self, node: ET.Element, xml_path: str, node_name: str) -> bool:
+        """
+        This method check if a note can be ignored (after it was handled).
+
+        :param node: XML node Object
+        :param xml_path: Path in global XML-file to this node
+        :param node_name: Name of the Node#
+        :returns: true if note should be ignored
+        """
+        return node_name in ('DataContainer', 'Data', 'Unit', 'Value', 'Dim1Spacing','Dim2Spacing')
+
 
     def prepare_tables(self):
         tables = []
-        table = self.append_table(tables)
-        key = ''
-        unit = {}
-        for line in self.file.fp.readlines():
-            row = line.decode('utf-8').strip()
-            if row.find('<Data key') >= 0:
-                key_start = row.find('key=')+5
-                key_end = row.find('"', key_start)
-                key = row[key_start:key_end]
-                value_start = (row.find('>')+1) if row.find('>') >= 0 else -1
-                value_end = row.find('</Data>')
-                table['metadata'][key] = row[value_start:value_end]
-            elif row.find('<Value>') >= 0:
-                value_start = row.find('<Value>')+7
-                value_end = row.find('</Value>')
-                table['metadata'][key] = row[value_start:value_end]
-            elif row.find('<Unit>') >= 0:
-                value_start = row.find('<Unit>') + 6
-                value_end = row.find('</Unit>')
-                unit[key] = row[value_start:value_end]
-            elif row.find('</Data>') >= 0:
-                value_start = (row.find('>') + 1) if row.find('>') >= 0 else -1
-                value_end = row.find('</Data>')
-                if value_end == 0:
-                    continue
-                table['metadata'][key] = row[value_start:value_end]
+        table = super().prepare_tables()[0]
+        tables.append(table)
 
-            if key in unit and table['metadata'][key] != '' and unit[key] not in table['metadata'][key]:
-                table['metadata'][key] += ' ' + unit[key]
+        table['metadata'].pop('Matrix', None)
 
         return tables
 
