26006658b753743835144e16a45004b561e38cba
Martin <m.starman@live.comdd>
2025-01-09 05:59:38 +0100

    Added ms_converter docker and connection

--- converter_app/app.py
+++ converter_app/app.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import dotenv
 import flask
 from str2bool import str2bool
-
+from urllib.parse import urlparse
 from converter_app.router import get_clients, setup_flask_routing
 from converter_app.utils import human2bytes
 
@@ -33,13 +33,14 @@ def create_app():
     # setup logging
     logging.basicConfig(level=os.getenv('LOG_LEVEL', 'INFO').upper(),
                         filename=os.getenv('LOG_FILE'))
-
+    parsed_uri = urlparse(os.getenv('MS_CONVERTER', 'http://127.0.0.1:5050'))
     # configure app
     app = flask.Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=os.getenv('SECRET_KEY'),
         PROFILES_DIR=os.getenv('PROFILES_DIR', 'profiles'),
         DATASETS_DIR=os.getenv('DATASETS_DIR', 'datasets'),
+        MS_CONVERTER='{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri),
         MAX_CONTENT_LENGTH=human2bytes(os.getenv('MAX_CONTENT_LENGTH', '64M')),
         CORS=str2bool(os.getenv('CORS', 'False').lower()),
         DEBUG=str2bool(os.getenv('DEBUG', 'False').lower()),
