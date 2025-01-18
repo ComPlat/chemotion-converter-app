137a2c21f7fb6bff688020e5d9a5ce877adab3b5
Martin <m.starman@live.comdd>
2025-01-18 10:58:49 +0100

    mzml added additional meta data

--- converter_app/readers/mzml.py
+++ converter_app/readers/mzml.py
@@ -17,6 +17,10 @@ class MSXmlReader(Reader):
     identifier = 'mzml_reader'
     priority = 8
 
+    data = ['i', 'mz']
+    data_2 = ['t_mz_set','transformed_mz_with_error' ]
+    ignore_fields = [None, '', 'obo_translator', 'transformed_peaks']
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._mz_xml_content = b''
@@ -65,34 +69,16 @@ class MSXmlReader(Reader):
                 f.write(self._mz_xml_content)
             runs = pymzml.run.Reader(tmp_file_name, build_index_from_scratch=True)
 
-
             tables = []
+            table = self.append_table(tables)
+
+            self._extract_meta_data(runs, table)
             for run in runs:
                 table = self.append_table(tables)
                 table['header'].append('-')
-                d = {k: getattr(run, k, '') for k in run.__dir__() if k[:1] != '_' and type(getattr(run, k, '')).__name__ != 'method'}
-                data = ['i', 'mz']
-                data_2 = ['t_mz_set','transformed_mz_with_error' ]
-                ignore_fields = ['obo_translator', 'transformed_peaks']
-                for key, val in d.items():
-                    if key not in data + data_2 + ignore_fields:
-                        if isinstance(val, ndarray):
-                            table.add_metadata(key, str(val.tolist()))
-                        elif isinstance(val, dict):
-                            for attrib_key, attrib_val in val.items():
-                                table.add_metadata(key + '.' + attrib_key, str(attrib_val))
-                        elif isinstance(val, tuple):
-                            for attrib_key, attrib_val in enumerate(val):
-                                table.add_metadata(key + '.' + str(attrib_key), attrib_val)
-                        elif isinstance(val, ET.Element):
-                            table.add_metadata(key + '.tag', val.tag)
-                            table.add_metadata(key + '.text', val.text.strip())
-                            for attrib_key, attrib_val in val.attrib.items():
-                                table.add_metadata(key + '.' + attrib_key, attrib_val)
-                        else:
-                            table.add_metadata(key, str(val))
+                d = self._extract_meta_data(run, table)
 
-                def add_data_table(table, data_set):
+                def add_data_table(table, data_set, d):
                     table['columns'] = []
                     for col_idx, data_key in enumerate(data_set):
 
@@ -100,16 +86,44 @@ class MSXmlReader(Reader):
                             while len(table['rows']) <= idx:
                                 table['rows'].append([])
                             table['rows'][idx].append(str(val))
-                        table['columns'].append({'key': col_idx, 'name': data_key})
+                        table['columns'].append({'key': str(col_idx), 'name': data_key})
 
-                add_data_table(table, data)
+                add_data_table(table, self.data, d)
                 table = self.append_table(tables)
                 table['header'].append(f'ID - {d["ID"]}')
-                add_data_table(table, data_2)
+                add_data_table(table, self.data_2, d)
 
             return tables
 
+    def _extract_meta_data(self, run, table):
+        d = {k: getattr(run, k, '') for k in run.__dir__() if
+             k[:1] != '_' and type(getattr(run, k, '')).__name__ != 'method'}
+        for key, val in d.items():
+            if key not in self.data + self.data_2 + self.ignore_fields:
+                if isinstance(val, ndarray):
+                    table.add_metadata(key, str(val.tolist()))
+                elif isinstance(val, dict):
+                    self._reade_meta_data_dict(key, table, val)
+                elif isinstance(val, tuple):
+                    for attrib_key, attrib_val in enumerate(val):
+                        table.add_metadata(key + '.' + str(attrib_key), str(attrib_val))
+                elif isinstance(val, ET.Element):
+                    table.add_metadata(key + '.tag', str(val.tag))
+                    if val.text is not None:
+                        table.add_metadata(key + '.text', val.text.strip())
+                    for attrib_key, attrib_val in val.attrib.items():
+                        table.add_metadata(key + '.' + attrib_key, str(attrib_val))
+                else:
+                    table.add_metadata(key, str(val))
+        return d
 
+    def _reade_meta_data_dict(self, key, table, val):
+        for attrib_key, attrib_val in val.items():
+            if isinstance(attrib_val, dict):
+                for attrib_key_i, attrib_val_i in attrib_val.items():
+                    table.add_metadata(key + '.' + str(attrib_key) + '.' + str(attrib_key_i), str(attrib_val_i))
+            else:
+                table.add_metadata(key + '.' + str(attrib_key), str(attrib_val))
 
 
 Readers.instance().register(MSXmlReader)
