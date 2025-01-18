a0c480ac9d80a8f93d9ee39922817888a2509002
Martin <m.starman@live.comdd>
2025-01-18 14:15:55 +0100

    raw timout increasing

--- etc_doc/entrypoint.sh
+++ etc_doc/entrypoint.sh
@@ -65,7 +65,7 @@ export LOG_FILE=/var/log/converter/application.log
 export GUNICORN_BIN=/srv/converter/env/bin/gunicorn
 export GUNICORN_WORKER=3
 export GUNICORN_PORT=8000
-export GUNICORN_TIMEOUT=350
+export GUNICORN_TIMEOUT=$TIMEOUT
 export GUNICORN_PID_FILE=/run/chemotion-converter/pid
 export GUNICORN_ACCESS_LOG_FILE=/var/log/converter/access.log
 export GUNICORN_ERROR_LOG_FILE=/var/log/converter/error.log
@@ -73,7 +73,7 @@ export MAX_CONTENT_LENGTH=250G
 
 pip install -r requirements/dev.txt
 # tail -f /dev/null
-gunicorn --bind 0.0.0.0:8000 "converter_app.app:create_app()"
+gunicorn --bind 0.0.0.0:8000 "converter_app.app:create_app()" --timeout $TIMEOUT
 
 
 
