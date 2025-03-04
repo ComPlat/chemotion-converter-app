bd40318b6f55ac0f3092708e44d1cdbaf7ff88eb
Adrian Herrmann <headri@web.de>
2025-03-04 15:24:16 +0100

    update copyright

--- converter_app/__init__.py
+++ converter_app/__init__.py
@@ -3,6 +3,6 @@ __version__ = '1.4.0'
 __author__ = 'Nicole Jung'
 __email__ = 'nicole.jung(at)kit.edu'
 __license__ = 'AGPL-3.0'
-__copyright__ = 'Copyright (c) 2020 Karlsruhe Institute for Technology (KIT)'
+__copyright__ = 'Copyright (c) 2020 - 2025 Karlsruhe Institute for Technology (KIT)'
 
 VERSION = __version__
