d71862b0c12e8e5bafbddc328eb22494d57ebf2b
David Herrmann <david.herrmann@kit.edu>
2025-04-11 11:13:33 +0200

    Fixing linefeed and comments in gcode file

--- converter_app/readers/gcode_reader.py
+++ converter_app/readers/gcode_reader.py
@@ -1,3 +1,5 @@
+import re
+
 from converter_app.readers import Readers
 from converter_app.readers.helper.base import Reader
 from converter_app.readers.helper.g_code_translator_package.GCode_Translator import GCodeTranslator
@@ -22,14 +24,21 @@ class GCodeReader(Reader):
         translator = GCodeTranslator()
         gcode_mapping = translator.init_mapping()
 
-        for line in self.file.fp.readlines(): # TODO: Not work if file line only ends with \r and not with \n
-            line = line.decode(self.file.encoding).rstrip()
+        all_lines = re.split(r'[\r\n]+', self.file.content.decode(self.file.encoding)) # for file line only ends with \r and not with \n
+
+        table = self.append_table(tables)
+        for line in all_lines:
+            line = line.rstrip()
             result = translator.explain_gcode_line(line, gcode_mapping, True, True)
             translator.translated_line_to_dict(result)
+            match = re.match(r';\s*([^:]+):(.+)', line)
+            if match:
+                table['metadata'][match.group(1)] = match.group(2)
         translator.clean_str_from_dict()
         for values in translator.sort_and_filter_dict(True):
             table = self.append_table(tables)
-            table['metadata'] = values
+            table['metadata'] = values # TODO: GCode comments as header
+            table["header"].append("Test")
         image_data = translator.get_preview_as_stream()
         if image_data:
             self.attachment_files.append({'preview.png': image_data})
