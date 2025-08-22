9b879e4b362d945d05cb8b1d9339b97d8ee8df1f
Martin Starman <martin.starman@kit.edu>
2025-08-22 10:58:41 +0200

    Updated schema_identifiers.py

--- converter_app/validation/schemas/schema_identifiers.py
+++ converter_app/validation/schemas/schema_identifiers.py
@@ -1,5 +1,43 @@
 from converter_app.validation.registry import SchemaRegistry
 
+ontology_properties = {
+    "properties": {
+        "id": {
+            "type": "string",
+        },
+        "iri": {
+            "type": "string",
+        },
+        "label": {
+            "type": "string",
+        },
+        "obo_id": {
+            "type": "string",
+        },
+        "ontology_name": {
+            "type": "string",
+        },
+        "ontology_prefix": {
+            "type": "string",
+        },
+        "short_form": {
+            "type": "string",
+        },
+        "description": {
+            "type": "array",
+            "items": {
+                "type": "string"
+            }
+        },
+        "type": {
+            "type": "string",
+        }
+    },
+    "additionalProperties": False,
+    "type": ["object", "null"],
+    "required": ["id" ,"iri" ,"label" ,"obo_id","ontology_name" ,"ontology_prefix" ,"short_form" ,"type"]
+}
+
 identifiers_schema = {
     "$schema": "https://json-schema.org/draft/2020-12/schema",
     "$id": "chemconverter://profile/identifiers/draft-01",
@@ -28,7 +66,10 @@ identifiers_schema = {
         },
         "value": {
             "type": "string"
-        }
+        },
+        "predicate": ontology_properties,
+        "subject": ontology_properties,
+        "datatype": ontology_properties,
     },
     "additionalProperties": True,
 
