dca3cef0514e977ee6693a97169b5061afa1d4f8
Martin Starman <martin.starman@kit.edu>
2025-11-07 13:43:20 +0100

    Added NTUPLES

--- converter_app/writers/jcampzip.py
+++ converter_app/writers/jcampzip.py
@@ -1,23 +1,24 @@
-import io
 import hashlib
+import io
 import json
-import zipfile
 import logging
+import zipfile
 
+from .base import Writer
 from .jcamp import JcampWriter
 
 logger = logging.getLogger(__name__)
 
 
-class JcampZipWriter(JcampWriter):
+class JcampZipWriter(Writer):
 
     suffix = '.zip'
     mimetype = 'application/zip'
 
     def __init__(self, converter):
+        super().__init__(converter)
         self.profile = converter.profile
         self.matches = converter.matches
-        self.tables = converter.tables
         self.zipbuffer = io.BytesIO()
 
     def process(self):
@@ -29,40 +30,48 @@ class JcampZipWriter(JcampWriter):
             'tables': []
         }
 
-        sha256_string = ''
-        sha512_string = ''
+        sha_strings = {'256': [], '512': []}
 
-        zf = zipfile.ZipFile(self.zipbuffer, 'w')
-        for table_id, table in enumerate(self.tables):
-            self.buffer = io.StringIO()
-            self.process_table(table)
-            string = self.buffer.getvalue()
+        with zipfile.ZipFile(self.zipbuffer, 'w') as zf:
 
-            file_name = 'data/table_{:02d}.jdx'.format(table_id + 1)
-            zf.writestr(file_name, string)
+            file_name = 'data/table.jdx'
+            jc = JcampWriter(self._converter)
+            tables = jc.process_ntuples_tables()
+            if len(tables) > 0:
+                string = self._add_table_to_zip(metadata, tables[0], file_name, zf, jc)
+                self._update_sha_strings(sha_strings, string, file_name)
 
-            sha256_string += '{} {}\n'.format(hashlib.sha256(string.encode()).hexdigest(), file_name)
-            sha512_string += '{} {}\n'.format(hashlib.sha512(string.encode()).hexdigest(), file_name)
+            for table_id, table in enumerate([t for t in self.tables if t.get('header', {}).get('DATA CLASS') != 'NTUPLES']):
+                file_name = f'data/table_{(table_id + 1):02d}.jdx'
+                jc = JcampWriter(self._converter)
+                jc.process_table(table)
+                string = self._add_table_to_zip(metadata, table, file_name, zf, jc)
+                self._update_sha_strings(sha_strings, string, file_name)
 
-            metadata['tablesCount'] += 1
-            metadata['tables'].append({
-                'fileName': file_name,
-                'header': table['header']
-            })
+            metadata_file_name = 'metadata/converter.json'
+            metadata_string = json.dumps(metadata, indent=2)
+            self._update_sha_strings(sha_strings, metadata_string, metadata_file_name)
+            zf.writestr(metadata_file_name, metadata_string)
 
-        metadata_file_name = 'metadata/converter.json'
-        metadata_string = json.dumps(metadata, indent=2)
-        sha256_string += '{} {}\n'.format(hashlib.sha256(metadata_string.encode()).hexdigest(), metadata_file_name)
-        sha512_string += '{} {}\n'.format(hashlib.sha512(metadata_string.encode()).hexdigest(), metadata_file_name)
-        zf.writestr(metadata_file_name, metadata_string)
+            # write bagit files (https://tools.ietf.org/id/draft-kunze-bagit-16.html)
+            zf.writestr('bagit.txt', 'BagIt-Version: 1.0\nTag-File-Character-Encoding: UTF-8\n')
+            zf.writestr('manifest-sha256.txt', '\n'.join(sha_strings['256']))
+            zf.writestr('manifest-sha512.txt', '\n'.join(sha_strings['512']))
 
-        # write bagit files (https://tools.ietf.org/id/draft-kunze-bagit-16.html)
-        zf.writestr('bagit.txt', 'BagIt-Version: 1.0\nTag-File-Character-Encoding: UTF-8\n')
-        zf.writestr('manifest-sha256.txt', sha256_string)
-        zf.writestr('manifest-sha512.txt', sha512_string)
 
-        # close zip file
-        zf.close()
+    def _update_sha_strings(self, sha_strings: dict[str, list[str]], content:str, filename: str):
+        sha_strings['256'].append(f'{hashlib.sha256(content.encode()).hexdigest()} {filename}')
+        sha_strings['512'].append(f'{hashlib.sha512(content.encode()).hexdigest()} {filename}')
 
-    def write(self):
+    def _add_table_to_zip(self, metadata, table, file_name, zf, jc):
+        string = jc.write()
+        zf.writestr(file_name, string)
+        metadata['tablesCount'] += 1
+        metadata['tables'].append({
+            'fileName': file_name,
+            'header': table['header']
+        })
+        return string
+
+    def write(self) -> bytes | str:
         return self.zipbuffer.getvalue()
