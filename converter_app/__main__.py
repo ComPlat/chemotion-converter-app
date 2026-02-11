eed6502e8eef3ce315c58ad04fda7a9d990334a2
Martin Starman <martin.starman@kit.edu>
2026-02-11 11:18:39 +0100

    fixed PR comments

--- converter_app/__main__.py
+++ converter_app/__main__.py
@@ -204,7 +204,9 @@ def migrate(force, profile):
         pf = Path(profile)
         Migrations().prepare_and_run_migration(pf, force)
     else:
-        Migrations().run_migration(create_app().config['PROFILES_DIR'], force)
+        profile_dir = create_app().config['PROFILES_DIR']
+        click.echo(f'Migrating all profiles in {profile_dir}')
+        Migrations().run_migration(profile_dir, force)
     click.echo("Migration complete!")
 
 
