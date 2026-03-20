545c92912bd5781f6cb9a07abbbe35b3f836bbaa
Martin Starman <martin.starman@kit.edu>
2026-03-20 10:40:06 +0100

    Added object to profiles, added validation. Updated RDF graph generation

--- converter_app/writers/rdf.py
+++ converter_app/writers/rdf.py
@@ -6,6 +6,21 @@ from converter_app.writers.base import Writer
 
 logger = logging.getLogger(__name__)
 
+default_value_predicate = {
+    "description": [
+        "Idiomatic property used for structured values."
+    ],
+    "id": "RDF:property:http://www.w3.org/1999/02/22-rdf-syntax-ns#value",
+    "iri": "http://www.w3.org/1999/02/22-rdf-syntax-ns#value",
+    "label": "value",
+    "namespace": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    "obo_id": "RDF:value",
+    "ontology_name": "rdf",
+    "ontology_prefix": "RDF",
+    "short_form": "RDF_value",
+    "type": "Property"
+}
+
 
 class RDFWriter(Writer):
     suffix = '.ttl'
@@ -16,12 +31,23 @@ class RDFWriter(Writer):
     def __init__(self, converter):
         super().__init__(converter)
         self._rdf_graph = Graph()
+        self.namespaces = set()
+        self.root_ontology = {}
+        self.subjects = []
+        self.predicates = []
+        self.datatypes = []
+        self.objects = []
+        self.instances = {}
 
     def _check_subject(self, i, subject_id, instance_name):
-        return self._check_types(i, {'id': subject_id}, 'subject') and i['subject'].get('subjectInstance') == instance_name
+        return self._check_types(i, {'id': subject_id}, 'subject') and i['subject'].get(
+            'subjectInstance') == instance_name
 
     def _check_predicate(self, i, p):
         return self._check_types(i, p, 'predicate')
+
+    def _check_object(self, i, p):
+        return self._check_types(i, p, 'object')
 
     def _check_datatype(self, i, d):
         return self._check_types(i, d, 'datatype')
@@ -44,78 +70,103 @@ class RDFWriter(Writer):
         return self._get_type_from_namespace_and_typename(namespace_objects[ontology['namespace']],
                                                           ontology['iri'].removeprefix(ontology['namespace']))
 
+    def _add_object(self, subject, predicate, instance_name, instance_type, namespace_objects):
+        instance_obj = self._get_type_from_namespace_and_typename(namespace_objects[self._namespace],
+                                                                  instance_name)
+        self._rdf_graph.add(
+            (instance_obj, RDF.type,
+             self._get_type_from_namespace_and_ontology(namespace_objects, instance_type)))
+        predicate_ont = next(p for p in self.predicates if p['id'] == predicate)
+        self._rdf_graph.add(
+            (subject, self._get_type_from_namespace_and_ontology(namespace_objects, predicate_ont),
+             instance_obj))
+        return instance_obj
+
     def process(self):
-        (root_ontology, subjects, predicates, datatypes, instances, namespaces, identifiers) = self._preprocess()
+        identifiers = self._preprocess()
 
         namespace_objects = {}
-        for ns in namespaces:
+        for ns in self.namespaces:
             ns_obj = Namespace(ns[0])
             self._rdf_graph.bind(ns[1], ns_obj)
             namespace_objects[ns[0]] = ns_obj
 
-        root_subject_instance = self._get_type_from_namespace_and_typename(namespace_objects[self._namespace], self._prepare_name(self._converter.profile.as_dict['title']))
+        root_subject_instance = self._get_type_from_namespace_and_typename(namespace_objects[self._namespace],
+                                                                           self._prepare_name(
+                                                                               self._converter.profile.as_dict[
+                                                                                   'title']))
 
         identifiers_without_subject = [i for i in identifiers if i.get('subject') is None]
 
-        self._add_props_to_subject(identifiers_without_subject, root_subject_instance, namespace_objects,
-                                   datatypes, predicates)
+        self._add_props_to_subject(identifiers_without_subject, root_subject_instance, namespace_objects)
 
         self._rdf_graph.add(
-            (root_subject_instance, RDF.type, self._get_type_from_namespace_and_ontology(namespace_objects, root_ontology)))
+            (root_subject_instance, RDF.type,
+             self._get_type_from_namespace_and_ontology(namespace_objects, self.root_ontology)))
 
-        for subject_id, instance_details in instances.items():
-            instance_ont = next((s for s in subjects if s['id'] == subject_id), None)
+        for subject_id, instance_details in self.instances.items():
+            instance_ont = next((s for s in self.subjects if s['id'] == subject_id), None)
             if instance_ont is not None:
                 for instance_dict in instance_details:
-                    instance_obj = self._get_type_from_namespace_and_typename(namespace_objects[self._namespace],
-                                                                              instance_dict['name'])
-                    self._rdf_graph.add(
-                        (instance_obj, RDF.type,
-                         self._get_type_from_namespace_and_ontology(namespace_objects, instance_ont)))
-                    predicate_ont = next(p for p in predicates if p['id'] == instance_dict['predicate'])
-                    self._rdf_graph.add(
-                        (root_subject_instance, self._get_type_from_namespace_and_ontology(namespace_objects, predicate_ont),
-                         instance_obj))
+                    instance_obj = self._add_object(root_subject_instance,
+                                     predicate=instance_dict['predicate'],
+                                     instance_name=instance_dict['name'],
+                                     instance_type=instance_ont,
+                                     namespace_objects=namespace_objects)
 
-                    identifiers_without_subject = [i for i in identifiers if self._check_subject(i, subject_id, instance_dict['name'])]
+                    identifiers_without_subject = [i for i in identifiers if
+                                                   self._check_subject(i, subject_id, instance_dict['name'])]
 
-                    self._add_props_to_subject(identifiers_without_subject, instance_obj, namespace_objects,
-                                               datatypes, predicates)
+                    self._add_props_to_subject(identifiers_without_subject, instance_obj, namespace_objects)
 
         for identifier in self._converter.profile.data['identifiers']:
             if identifier['optional']:
                 if identifier['subject']:
-                    subjects.append(identifier['subject'])
+                    self.subjects.append(identifier['subject'])
 
-    def _add_props_to_subject(self, identifiers_for_subject, instance_obj, namespace_objects, datatypes, predicates):
+    def _add_props_to_subject(self, identifiers_for_subject, instance_obj, namespace_objects):
         for i in identifiers_for_subject:
-            predicate_ont = next((p for p in predicates if self._check_predicate(i, p)), None)
+            predicate_ont = next((p for p in self.predicates if self._check_predicate(i, p)), None)
+            object_out = next((p for p in self.objects if self._check_object(i, p)), None)
+
+            if object_out is None:
+                return
+            if i.get('datatype') is not None:
+                datatype_ont = next((d for d in self.datatypes if self._check_datatype(i, d)))
+                value_literal = Literal(i['value'],
+                                 datatype=self._get_type_from_namespace_and_ontology(namespace_objects,
+                                                                                     datatype_ont))
+            else:
+                value_literal = Literal(i['value'])
+
             if predicate_ont is not None:
-                if i.get('datatype') is not None:
-                    datatype_ont = next((d for d in datatypes if self._check_datatype(i, d)))
-                    object = Literal(i['value'],
-                                     datatype=self._get_type_from_namespace_and_ontology(namespace_objects,
-                                                                                         datatype_ont))
-                else:
-                    object = Literal(i['value'], datatype=XSD.string)
+                item = self._add_object(instance_obj, predicate=i.get('predicate', {}).get('id'), instance_name=f'{object_out['label']}_instance', instance_type=object_out, namespace_objects=namespace_objects)
+
+
                 self._rdf_graph.add(
-                    (instance_obj, self._get_type_from_namespace_and_ontology(namespace_objects, predicate_ont),
-                     object))
+                    (item, self._get_type_from_namespace_and_ontology(namespace_objects, default_value_predicate),
+                     value_literal))
+            else:
+                self._rdf_graph.add(
+                    (instance_obj, self._get_type_from_namespace_and_ontology(namespace_objects, object_out),
+                     value_literal))
 
     def _preprocess(self):
-        namespaces = {(self._namespace, self._namespace_name)}
         profile = self._converter.profile.as_dict
-        root_ontology = profile['rootOntology']
-        subjects = profile['subjects']
-        predicates = profile['predicates']
-        datatypes = profile['datatypes']
-        instances = profile['subjectInstances']
+        self.namespaces = {(self._namespace, self._namespace_name)}
+        self.root_ontology = profile['rootOntology']
+        self.subjects += profile['subjects']
+        self.predicates += profile['predicates']
+        self.datatypes += profile['datatypes']
+        self.objects += profile['objects']
+        self.instances.update(profile['subjectInstances'])
         identifiers = [i['identifier'] | i['result'] for i in self._converter.matches if i['result']]
+        if any(x.get('predicate', False) for x in identifiers):
+            self.predicates.append(default_value_predicate)
+        for element in [self.root_ontology] + self.subjects + self.predicates + self.datatypes + self.objects:
+            self.namespaces.add((element['namespace'], element['ontology_name']))
 
-        for element in [root_ontology] + subjects + predicates + datatypes:
-            namespaces.add((element['namespace'], element['ontology_name']))
-
-        return root_ontology, subjects, predicates, datatypes, instances, namespaces, identifiers
+        return identifiers
 
     def write(self):
         return self._rdf_graph.serialize(format="turtle").encode()
