3caa10cb74bd6c36ae9f0aed591f9302c1d7852d
StarmanMartin <m.starman@live.com>
2026-04-16 10:26:09 +0200

    224 replace data file check identifier (#225)
    * Added save and close option
    Added add inout file option
    Added dropdown switch input file
    
    * Added save and close option
    Added add inout file option
    Added dropdown switch input file
    
    * import fix
    
    * Migrated test profiles

---
+++ converter_app/profile_migration/20260401123327_migration.py
@@ -0,0 +1,34 @@
+from converter_app.profile_migration import ProfileMigration
+
+
+class ProfileMigrationScript(ProfileMigration):
+    """
+    Transforms data to a list. (Multiple input files)
+    """
+
+    def up(self, profile: dict):
+        """
+        Updates the profile.
+        """
+
+        if not (isinstance(profile['data'], list)):
+            profile['data'] = [profile['data']]
+
+    def to_be_applied_after_migration(self) -> str:
+        """
+        Uses the unique identifier of the last script to tell the migration process in which order
+        the migration script must be applied.
+        """
+
+        return '20260113143334'
+
+    @property
+    def identifier(self):
+        """
+        A unique identifier for the migration script.
+        This identifier will be used:
+         A) in the Profile to determine if a migration has been applied to a certain Profile.
+         B) must be returned by the to_be_applied_after_migration method of the following script
+        """
+
+        return '20260401123327'
