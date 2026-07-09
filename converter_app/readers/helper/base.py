0abfe18ba0d3d5f4b513e7b7e85531d0cfb09e35
StarmanMartin <m.starman@live.com>
2026-07-09 10:17:43 +0200

    Fix is tar ball (#239)
    * fix
    
    * fix run reaction identifier
    
    * fixed tests
    
    * fixed tests
    
    * fixed new version in test

--- converter_app/readers/helper/base.py
+++ converter_app/readers/helper/base.py
@@ -95,7 +95,7 @@ class Reader:
         self.tables = None
         self.file = file
         self.file_content = tar_content
-        self.is_tar_ball = len(tar_content) > 0,
+        self.is_tar_ball = len(tar_content) > 0
         self._ontology = None
 
     @property
